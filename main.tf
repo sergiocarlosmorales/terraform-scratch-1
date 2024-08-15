2f4d28754567d4c3f2e85b0f8bb56a04a7c3a33f
Sergio Carlos Morales Angeles <sergiocarlos@us.ibm.com>
2024-08-15 10:31:20 -0400

    Test

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.68.1"
+    }
+  }
+}
+
+variable "region" {
+  description = "Region where to deploy the VPC"
+  type        = string
+  default     = "us-south"
+}
+
+variable "resource_group" {
+  description = "Resource group ID where to deploy the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "api_key" {
+  description = "API key"
+  type        = string
+}
+
+# Configure the IBM Provider
+provider "ibm" {
+  region = var.region
+  ibmcloud_api_key = var.api_key
+}
+
+# Create a VPC
+resource "ibm_is_vpc" "created_vpc_sangeles_fun" {
+  name = var.vpc_name
+  resource_group = var.resource_group
+}

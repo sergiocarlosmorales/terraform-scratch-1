8e0e6a05d6a06b318f3a101484e671875712b1d0
Sergio Carlos Morales Angeles <sergiocarlos@us.ibm.com>
2024-08-27 15:32:17 -0400

    work

--- main.tf
+++ main.tf
@@ -24,6 +24,10 @@ variable "bm_cpu_keyname" {
   description = "Product keyName for the CPU (e.g., INTEL_INTEL_XEON_5218_2_30)"
   type        = string
 }
+variable "bm_uplink_speed" {
+  description = "Uplink speed in Mbits (e.g. 1000 for 1Gbps)"
+  type        = number
+}
 
 resource "ibm_compute_bare_metal" "created_bare_metal" {
   hostname         = var.bm_hostname
@@ -33,7 +37,7 @@ resource "ibm_compute_bare_metal" "created_bare_metal" {
   process_key_name = var.bm_cpu_keyname
   memory           = var.bm_ram
   os_key_name      = "OS_CENTOS_STREAM_9_X_64_BIT"
-  network_speed    = 1000
+  network_speed    = var.bm_uplink_speed
   public_bandwidth = 5000
   disk_key_names   = ["HARD_DRIVE_3_8TB_SSD_3DWPD", "HARD_DRIVE_3_8TB_SSD_3DWPD", "HARD_DRIVE_3_8TB_SSD_3DWPD"]
   hourly_billing   = false

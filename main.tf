e5a970c97983313d99af80a4fba12485bc57b044
Sergio Carlos Morales Angeles <sergiocarlos@us.ibm.com>
2025-11-11 20:50:31 -0600

    Migrating computer

--- main.tf
+++ main.tf
@@ -7,44 +7,44 @@ terraform {
   }
 }
 
-variable "bm_dc" {
-  description = "DC for the Bare Metal"
-  type        = string
-  default     = "dal13"
-}
-variable "bm_hostname" {
-  description = "Hostname for the Bare Metal"
-  type        = string
-}
-variable "bm_ram" {
-  description = "How much RAM to assign"
-  type        = number
-}
-variable "bm_cpu_keyname" {
-  description = "Product keyName for the CPU (e.g., INTEL_INTEL_XEON_5218_2_30)"
-  type        = string
-}
-variable "bm_uplink_speed" {
-  description = "Uplink speed in Mbits (e.g. 1000 for 1Gbps)"
-  type        = number
-}
-
-resource "ibm_compute_bare_metal" "created_bare_metal" {
-  hostname         = var.bm_hostname
-  domain           = "softlayer.local"
-  datacenter       = var.bm_dc
+resource ibm_compute_bare_metal "classic_bm_6764827786fdf" {
+  hourly_billing = false
+  hostname = "host0"
+  domain = "sl0.local"
+  os_key_name = "OS_CENTOS_STREAM_9_X_64_BIT"
+  datacenter = "dal14"
   package_key_name = "DUAL_INTEL_XEON_PROC_CASCADE_LAKE_SCALABLE_FAMILY_12_DRIVES"
-  process_key_name = var.bm_cpu_keyname
-  memory           = var.bm_ram
-  os_key_name      = "OS_CENTOS_STREAM_9_X_64_BIT"
-  network_speed    = var.bm_uplink_speed
+  process_key_name = "INTEL_INTEL_XEON_4210_2_20"
+  memory = 128
+  network_speed = 1000
   public_bandwidth = 5000
-  disk_key_names   = ["HARD_DRIVE_3_8TB_SSD_3DWPD", "HARD_DRIVE_3_8TB_SSD_3DWPD", "HARD_DRIVE_3_8TB_SSD_3DWPD"]
-  hourly_billing   = false
+  private_network_only = false
+  unbonded_network = false
+  disk_key_names = [
+    "HARD_DRIVE_2_00_TB_SATA_2",
+    "HARD_DRIVE_2_00_TB_SATA_2",
+    "HARD_DRIVE_960GB_SSD",
+    "HARD_DRIVE_960GB_SSD",
+    "HARD_DRIVE_960GB_SSD",
+    "HARD_DRIVE_960GB_SSD"
+  ]
   storage_groups {
-    array_type_id = 2
-    hard_drives = [0, 1]
-    array_size  = 3800
+    array_type_id = 1
+    hard_drives = [
+      0,
+      1
+    ]
+    array_size = 4000
     partition_template_id = 1144
+  }
+  storage_groups {
+    array_type_id = 5
+    hard_drives = [
+      2,
+      3,
+      4,
+      5
+    ]
+    array_size = 1920
   }
 }
